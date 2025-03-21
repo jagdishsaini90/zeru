7c7406012204d9157cbb83bd2c4041fb77f87992
jagdishsaini90 <jsaini8941@gmail.com>
2025-03-21 18:16:08 +0530

    added responsive

--- components/tabs.js
+++ components/tabs.js
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function Tabs({ tabs = [], activeTab, setActiveTab }) {
   return (
-    <div className="flex w-full md:w-auto">
+    <div className="flex justify-between w-full md:w-auto">
       {tabs.map((tab) => (
         <button
           key={tab}
           onClick={() => setActiveTab(tab)}
-          className={`whitespace-nowrap cursor-pointer px-6 py-1.5 rounded-full text-sm font-medium transition shrink-0 ${
+          className={`whitespace-nowrap cursor-pointer px-3 md:px-6 py-1.5 rounded-full text-sm font-medium transition shrink-0 ${
             activeTab === tab
               ? "bg-gray-100 text-black"
               : "text-gray-500 hover:text-black"
